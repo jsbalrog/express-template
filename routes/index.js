0071843836db9735da801b6c6393c995bad039b3
jsbalrog <edwardtjenkins@gmail.com>
2014-10-23 12:26:33 -0600

    Removed unnecessary folder

--- routes/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(app) {
-  require('./users')(app);
-  // Otherwise, go to the home page
-  require('./main')(app);
-};
--- routes/main.js
+++
@@ -1,5 +0,0 @@
-var ctrl = require('../app_server/main');
-
-module.exports = function(app) {
-  app.use('/', ctrl.index);
-};
